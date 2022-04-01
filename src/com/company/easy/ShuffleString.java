ea2996ebfb7d6afd855d3085549ff71461623c19
Admin <boynazarovjalil@gmail.com>
2022-04-01 23:30:54 +0500

    new

---
+++ src/com/company/easy/ShuffleString.java
@@ -0,0 +1,30 @@
+package com.company.easy;
+
+import java.util.*;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
+        System.out.println(restoreString(s, indices));
+
+
+    }
+
+    public static String restoreString(String s, int[] indices) {
+        Map<Integer, String> map = new HashMap<>();
+
+        for (int i = 0; i < indices.length; i++) {
+            map.put(indices[i], String.valueOf(s.charAt(i)));
+        }
+
+        map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey());
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < map.size(); i++) {
+            builder.append(map.get(i));
+        }
+        return builder.toString();
+    }
+}
