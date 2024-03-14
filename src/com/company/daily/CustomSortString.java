3cb0b0fce839361dff7503d350688a863abf1c3d
jalil2020 <boynazarovjalil@gmail.com>
2024-03-14 08:31:44 +0500

    splitNum

---
+++ src/com/company/daily/CustomSortString.java
@@ -0,0 +1,7 @@
+package com.company.daily;
+
+public class CustomSortString {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/misol/SplitNum.kt
@@ -0,0 +1,12 @@
+package com.company.misol
+
+import java.sql.DriverManager.println
+
+
+fun main() {
+    println(splitNum(2323).toString())
+}
+fun splitNum(num: Int): Int {
+
+    return num
+}
